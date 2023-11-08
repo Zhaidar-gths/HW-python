d0ea3ddaa92da4880176e570d92e6d25fae4e183
Zhaidar <149644540+Zhaidar-gths@users.noreply.github.com>
2023-11-08 11:01:11 +0600

    home work

---
+++ lists_hw.py
@@ -0,0 +1,189 @@
+#LEVEL 1
+#task 1
+# word = list ()
+# empty_list = list ()
+# # print(len(empty_list))
+# print(empty_list)
+
+
+#task 2
+# countries = ['Kanada','Switzerland','Monaco','Singapore','Luxembourg']
+# print(countries)
+
+
+#task 3
+# countries = ['Kanada','Switzerland','Monaco','Singapore','Luxembourg']
+# print(len(countries))
+
+
+#task 4
+# countries = ['Kanada','Switzerland','Monaco','Singapore','Luxembourg']
+# first_country = countries [0]
+# print (first_country)
+# third_country = countries [2]
+# print (third_country)
+# fifth_country = countries [4]
+# print (fifth_country)
+
+
+#task 5
+# mixed_data_types = {'name':'Malika','age':'17','height':'162','marital status':'single','address':'Turkebaeva 59'}
+# print(mixed_data_types)
+
+
+#task 6
+# it_companies = ['Facebook','Google','Microsoft','Apple','IBM','Oracle','Amazon']
+# print(it_companies)
+
+
+#task 7
+# fruits = ['banana', 'orange', 'mango', 'lemon']
+# print(fruits)
+
+
+#task 8
+# it_companies = ['Facebook','Google','Microsoft','Apple','IBM','Oracle','Amazon']
+# print(len(it_companies))
+
+
+#task 9
+# it_companies = ['Facebook','Google','Microsoft','Apple','IBM','Oracle','Amazon']
+# first_company = it_companies [0]
+# print(first_company)
+# forth_company = it_companies [3]
+# print(forth_company)
+# seventh_company = it_companies [6]
+# print(seventh_company)
+
+
+#task 10
+# it_companies = ['Facebook','Google','Microsoft','Apple','IBM','Oracle','Amazon']
+# it_companies [-1] = 'Alphabet' 
+# print(it_companies)
+
+
+#task 11
+# it_companies = ['Facebook','Google','Microsoft','Apple','IBM','Oracle','Amazon']
+# it_companies [0] = 'Alphabet'
+# print (it_companies)
+
+
+#task 12
+# it_companies = ['Facebook','Google','Microsoft','Apple','IBM','Oracle','Amazon']
+# it_companies [4] = 'Alphabet'
+# print (it_companies)
+ 
+ 
+#task 13
+# it_companies = ['Facebook','Google','Microsoft','Apple','IBM','Oracle','Amazon']
+# print(it_companies.upper())
+
+
+# #task 14
+# it_companies = ['Facebook','Google','Microsoft','Apple','IBM','Oracle','Amazon']
+
+
+#task 15
+# it_companies = ['Facebook','Google','Microsoft','Apple','IBM','Oracle','Amazon']
+# does_exist = 'Facebook' in it_companies 
+# print(does_exist)
+
+
+#task 16
+# it_companies = ['Facebook','Google','Microsoft','Apple','IBM','Oracle','Amazon']
+# it_companies.sort()
+# print(it_companies)
+
+
+#task 17
+# it_companies = ['Facebook','Google','Microsoft','Apple','IBM','Oracle','Amazon']
+# it_companies.reverse()
+# print(it_companies)
+
+
+#task 18
+# it_companies = ['Facebook','Google','Microsoft','Apple','IBM','Oracle','Amazon']
+# first_company = it_companies[3:]
+# print(first_company)
+
+
+#task 19
+# it_companies = ['Facebook','Google','Microsoft','Apple','IBM','Oracle','Amazon']
+# first_company = it_companies[:-3]
+# print(first_company)
+
+
+#task 20
+# it_companies = ['Facebook','Google','Microsoft','Apple','IBM','Oracle','Amazon']
+# it_companies.remove('Apple')
+# print(it_companies)
+
+
+#task 21
+# it_companies = ['Facebook','Google','Microsoft','Apple','IBM','Oracle','Amazon']
+# it_companies.remove('Facebook')
+# print(it_companies)
+
+
+#task 22
+# it_companies = ['Facebook','Google','Microsoft','Apple','IBM','Oracle','Amazon']
+# it_companies.remove('Apple')
+# print(it_companies)
+
+
+#task 23
+# it_companies = ['Facebook','Google','Microsoft','Apple','IBM','Oracle','Amazon']
+# it_companies.remove('Amazon')
+# print(it_companies)
+
+
+#task 24
+# it_companies = ['Facebook','Google','Microsoft','Apple','IBM','Oracle','Amazon']
+# it_companies.remove('it_companies')
+# print(it_companies)
+
+# lst = ['item1', 'item2']
+# lst.pop() # last item lst.pop(index)
+
+
+#task 25
+# it_companies = ['Facebook','Google','Microsoft','Apple','IBM','Oracle','Amazon']
+
+
+
+#task 26
+# front_end = ['HTML', 'CSS', 'JS', 'React', 'Redux']
+# back_end = ['Node','Express', 'MongoDB']
+# integers = front_end + back_end
+# print(integers)
+
+
+#task 27
+# front_end = ['HTML', 'CSS', 'JS', 'React', 'Redux']
+# back_end = ['Node','Express', 'MongoDB']
+# integers = front_end + ba
+
+#1
+# ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
+# ages.sort()
+# print(ages)
+# print("min=",min(ages))
+# print("max=",max(ages))
+#2
+# ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
+# print("min=",min(ages))
+# print("max=",max(ages))
+#3
+# countries = [‘China’, ‘Russia’, ‘USA’, ‘Finland’, ‘Sweden’, ‘Norway’, ‘Denmark’]
+# length = len(countries)
+#   middle_index = length // 2
+#   return countries [middle_index]
+ 
+ 
+#3
+# countries = ['China','Russia','USA','Finland','Sweden','Norway','Denmark']
+# first_country, second_country, third_country, *rest = countries
+# print(first_country)
+# print(second_country)
+# print(third_country)
+# print(rest)
---
+++ str_hw.py
@@ -0,0 +1,138 @@
+# #Level 1
+
+# #Task 2
+# #Day 2: 30 Days of python programming
+
+
+# #Task 3
+# first_name='Zhaidar'
+# print(first_name)
+
+# #Task 4
+# Last_name='Maulen'
+# print(Last_name)
+
+# #Task 5
+# first_name='Zhaidar'
+# last_name=' Maulenkyzy'
+# spase=''
+# ful_name = first_name + spase + last_name
+# print(ful_name)
+
+# #Task 6
+# country='Kazakhstan'
+# print(country)
+
+# #Task 7
+# city='Almaty'
+# print(city)
+
+# # Task 8
+# age='16'
+# print(age)
+
+# # Task 9
+# year='2006'
+# print(year)
+
+# # Task 10
+# is_married = 'false'
+# print(is_married)
+
+# # Task 11
+# is_true="i don't have a cat"
+# print(is_true)
+
+# # Task 12
+# is_light='today is light'
+# print(is_light)
+
+
+
+# # Level 2
+
+# # Task 1
+# first_name='Zhaidar'
+# last_name=' Maulenkyzy'
+# country='Kazakhstan'
+# age='16'
+# year='2006'
+# is_married = False
+# print(type(first_name))
+# print(type(last_name))
+# print(type(country))
+# print(type(16))
+# print(type(2006))
+# print(type(False))
+
+
+# # Task 2
+# print(len('Zhaidar'))
+
+
+# # Task 3
+# first_name='Zhaidar'
+# last_name=' Maulenkyzy'
+# spase=''
+# print(len(first_name) > len(last_name))
+
+# Task 4
+# num_one = 5
+# num_two = 4
+# #1
+# total = num_one + num_two
+# print('total:', total)
+
+# #2
+# diff = num_one - num_two
+# print('difference:', diff)
+
+# #3
+# product = num_one * num_two
+# print('product:', product)
+
+
+# #4
+# div = num_one / num_two
+# print('division:', div)
+
+
+# #5
+# remainder =num_one % num_two
+# print('remainder:', remainder)
+
+# #6
+# exp = num_one**num_two
+# print('exponentiation:', exp)
+
+# #7
+# floor_division = num_one // num_two
+# print('floor_division:', floor_division)
+
+
+# # Task 5
+# #1
+# radius = 30 
+# area_of_circle = 3.14 * radius ** 2 
+# print('Area of a circle:', area_of_circle)
+
+# #2
+# radius=30
+# circum_of_circle=2*3,14*30
+# print(circum_of_circle)
+
+#3
+height=float(input("height:"))
+width=float(input("width:"))
+end=height*width
+print(end)
+
+# Task 6
+First_name=input("Enter your First_name:")
+Last_name=input("enter your last_name:")
+country=input("enter your country:")
+age= input("enter age:")
+print(First_name)
+print(Last_name)
+print(country)
+print(age)
